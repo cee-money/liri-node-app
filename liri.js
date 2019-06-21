894854c00d4fb928d9cdd102ba9e9f4b52283294
cmoney <moneymaker.carolyn@gmail.com>
2019-06-20 18:02:40 -0600

    switch case to enable commands in liri.js

--- liri.js
+++ liri.js
@@ -1,3 +1,40 @@
-require("dotenv").config();
-  var keys = require("./keys.js");
-  var spotify = new Spotify(keys.spotify);
+// require("dotenv").config();
+//   var keys = require("./keys.js");
+//   var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var searchName = process.argv[3];
+var result;
+
+function getSong() {
+    console.log("song");
+};
+
+function getMovie() {
+    console.log("movie");
+};
+
+function getConcert() {
+    console.log("concert");
+};
+
+function getTxt() {
+    console.log("text");
+};
+
+switch(command) {
+    case "movie-this": 
+        result = searchName;
+        break;
+    case "concert-this": 
+        result = searchName;
+        break;
+    case "spotify-this-song": 
+        result = searchName;
+        break;
+    case "do-what-it-says": 
+        result = searchName;
+        break;
+}
+console.log(result);
+
