a970fa4ec8bd52595c697bd2ef16e7e150f75520
cmoney <moneymaker.carolyn@gmail.com>
2019-06-25 18:37:13 -0600

    added append file function, updated readMe

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # liri-node-app
-Deployed version: https://cee-money.github.io/liri-node-app/
+Deployed version (please note that this app is meant to be run in the user's Terminal and not in the user's browser): 
+https://cee-money.github.io/liri-node-app/
 
 LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI is a command line node app that takes in parameters and gives back data. LIRI searches Spotify for songs, Bands in Town for concerts, and OMDB for movies.
 
@@ -49,9 +50,28 @@ LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and R
 
 -If the user enters " node liri.js do-what-it-says " into the command line, LIRI takes the text inside of random.txt and then uses it to call one of LIRI's commands.
 
+Role in development: I created this app in it's entirety for a coding boot camp assignment
 
+Overview of how the app is organized:
+-Global variables are declared that: 
+    -Store API keys in a hidden file and allow those to be accessed by LIRI
+    -Implement various NPMs and the file system module
+    -Store user inputs
+-Functions are defined:
+    -One searches the Spotify API for a song by name and renders information to the terminal, unless the user inputs no song title - then it will render to the terminal information about "The Sign" by Ace of Base.
+    -Another searches the OMDB API for a movie by name and renders information to the terminal, unless the user inputs no movie title - then it will render to the terminal information about "Mr. Nobody".
+    -Another searches the Bands in Town API for an artist by name and renders information to the terminal about upcoming concerts, unless the user inputs no artist - then it will ask the user to enter an artist.
+    -Another reads the random.txt file, turns the text therein into an array, grabs the array members to create a command and serach term, then uses those (with the help of a switchcase) to call the appropriate function 
+    -Another writes the results from any search to a file called log.txt 
+-A switchcase is defined to take the user's command input to run the appropriate function on the user's search term input. If the user's command is not stated as a case, the user will be directed to write a valid command.
 
-
-
-
-
+New technologies used:
+-NPMs
+    Spotify
+    OMDB
+    Bands in Town
+    Moment
+-Hidden keys
+-File system module (read file and append file)
+-Template literals
+- .slice() method
--- liri.js
+++ liri.js
@@ -1,15 +1,23 @@
+// Storing Spotify keys in a hidden file
 require("dotenv").config();
-var fs = require("fs");
-var Spotify = require('node-spotify-api');
+
+// The keys file exports and object that refers to my hidden Spotify keys in the .env file
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+
+// Implementing various modules and NPMs
+var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
 moment().format();
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
+// Stored user inputs as variables
 var command = process.argv[2];
 var searchTerm = process.argv.slice(3);
+var renderInfo;
 
+// This function searches the Spotify API for a song by name and renders information to the terminal, unless the user inputs no song title - then it will render to the terminal information about "The Sign" by Ace of Base.
 function getSong() {
 
     if (process.argv.length >= 4 || process.argv[2] == "do-what-it-says") { 
@@ -18,11 +26,13 @@ function getSong() {
             if (err) {
             return console.log('Error occurred: ' + err);
             }
+
+        renderInfo = `\nArtist: ${data.tracks.items[0].artists[0].name}\nSong: ${data.tracks.items[0].name}\nLink to song: ${data.tracks.items[0].href}\nAlbum: ${data.tracks.items[0].album.name}\n\n------------------------\n`;
         
-        console.log(`\nArtist: ${data.tracks.items[0].artists[0].name}`);
-        console.log(`\nSong: ${data.tracks.items[0].name}`);
-        console.log(`\nLink to song: ${data.tracks.items[0].href}`);
-        console.log(`\nAlbum: ${data.tracks.items[0].album.name}\n`);  
+        console.log(renderInfo);
+        
+        writeFile();
+
         });
     } else {
 
@@ -31,30 +41,31 @@ function getSong() {
               return console.log('Error occurred: ' + err);
             }
         
-        console.log(`\nYou forgot to enter a song name, so here's some info on ${data.tracks.items[5].name} by ${data.tracks.items[5].artists[0].name}:`);
-        console.log(`\nArtist: ${data.tracks.items[5].artists[0].name}`);
-        console.log(`\nSong: ${data.tracks.items[5].name}`);
-        console.log(`\nLink to song: ${data.tracks.items[5].href}`);
-        console.log(`\nAlbum: ${data.tracks.items[5].album.name}\n`); 
+        renderInfo = `\nYou forgot to enter a song name, so here's some info on ${data.tracks.items[5].name} by ${data.tracks.items[5].artists[0].name}:\nArtist: ${data.tracks.items[5].artists[0].name}\nSong: ${data.tracks.items[5].name}\nLink to song: ${data.tracks.items[5].href}\nAlbum: ${data.tracks.items[5].album.name}\n\n------------------------\n`;
+        
+        console.log(renderInfo);
+
+        writeFile();
+      
         });
     }
 };
 
+// This function searches the OMDB API for a movie by name and renders information to the terminal, unless the user inputs no movie title - then it will render to the terminal information about "Mr. Nobody".
 function getMovie() {
 
     var queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy"
 
-    if (process.argv.length >= 4) { 
+    if (process.argv.length >= 4 || process.argv[2] == "do-what-it-says") { 
 
         axios.get(queryUrl).then(function(response) {
-            console.log(`\nYou searched for ${response.data.Title}. Here are some facts about it:`);
-            console.log(`\nReleased in: ${response.data.Year}`);
-            console.log(`\nIMDB rating: ${response.data.Ratings[0].Value}`);
-            console.log(`\nRotten Tomatoes rating: ${response.data.Ratings[1].Value}`);
-            console.log(`\nProduced in: \n ${response.data.Country.split(",").join("\n")}`);
-            console.log(`\nAvailable in: \n ${response.data.Language.split(",").join("\n")}`);
-            console.log(`\nPlot summary: ${response.data.Plot}`);
-            console.log(`\nLead actors: \n ${response.data.Actors.split(",").join("\n")}\n`);
+
+            renderInfo = `\nYou searched for ${response.data.Title}. Here are some facts about it:\n\nReleased in: ${response.data.Year}\n\nIMDB rating: ${response.data.Ratings[0].Value}\n\nRotten Tomatoes rating: ${response.data.Ratings[1].Value}\n\nProduced in: \n ${response.data.Country.split(",").join("\n")}\n\nAvailable in: \n ${response.data.Language.split(",").join("\n")}\n\nPlot summary: ${response.data.Plot}\n\nLead actors: \n ${response.data.Actors.split(",").join("\n")}\n\n------------------------\n`;
+
+            console.log(renderInfo);
+
+            writeFile();
+
         }).catch(function(error) {
             if (error.response) {
             // The request was made and the server responded with a status code
@@ -80,41 +91,47 @@ function getMovie() {
         var queryUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
 
         axios.get(queryUrl).then(function(response) {
-            console.log(`\nYou forgot to enter a movie title, so here's some info on ${response.data.Title}:`);
-            console.log(`\nReleased in: ${response.data.Year}`);
-            console.log(`\nIMDB rating: ${response.data.Ratings[0].Value}`);
-            console.log(`\nRotten Tomatoes rating: ${response.data.Ratings[1].Value}`);
-            console.log(`\nProduced in: \n ${response.data.Country.split(",").join("\n")}`);
-            console.log(`\nAvailable in: \n ${response.data.Language.split(",").join("\n")}`);
-            console.log(`\nPlot summary: ${response.data.Plot}`);
-            console.log(`\nLead actors: \n ${response.data.Actors.split(",").join("\n")}`);
+
+            renderInfo = `\nYou forgot to enter a movie title, so here's some info on ${response.data.Title}:\n\nReleased in: ${response.data.Year}\n\nIMDB rating: ${response.data.Ratings[0].Value}\n\nRotten Tomatoes rating: ${response.data.Ratings[1].Value}\n\nProduced in: \n ${response.data.Country.split(",").join("\n")}\n\nAvailable in: \n ${response.data.Language.split(",").join("\n")}\n\nPlot summary: ${response.data.Plot}\n\nLead actors: \n ${response.data.Actors.split(",").join("\n")}\n\n------------------------\n`;
+
+            console.log(renderInfo);
+
+            writeFile();
+
         });
     };
 };
 
-
+// This function searches the Bands in Town API for an artist by name and renders information to the terminal about upcoming concerts, unless the user inputs no artist - then it will ask the user to enter an artist.
 function getConcert() {
     searchTerm = process.argv.slice(3).join("+");
     var queryUrl = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
 
-    if (process.argv.length >= 4) { 
+    if (process.argv.length >= 4 || process.argv[2] == "do-what-it-says") { 
 
         axios.get(queryUrl).then(function(response) {
 
             if (response.data.length == 0) {
-                console.log("\nSorry, there are no upcoming shows for this artist.\n");
+
+                renderInfo = `\nSorry, there are no upcoming shows for ${searchTerm}.`;
+
+                console.log(renderInfo);
+
+                writeFile();
 
             } else {
-                console.log(`\nYou searched for ${searchTerm}. Here are some upcoming shows:`);
 
                 for (var i = 0; i < response.data.length; i++) {
                     var concertDate = response.data[i].datetime.split("T").slice(0,1);
                     var dateFormat = "YYYY-MM-DD";
                     var formattedDate = moment(concertDate, dateFormat);
 
-                    console.log(`\nVenue: ${response.data[i].venue.name}`);
-                    console.log(`Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}`);
-                    console.log(`Date: ${formattedDate.format("MM/DD/YYYY")}\n`);
+                    renderInfo = `\nUpcoming show for ${searchTerm.split("+").join(" ")}:\nVenue: ${response.data[i].venue.name} | Location: ${response.data[i].venue.city}, ${response.data[i].venue.region} | Date: ${formattedDate.format("MM/DD/YYYY")}\n\n------------------------\n`;
+
+                    console.log(renderInfo);
+
+                    writeFile();
+
                 };
             };
         }).catch(function(error) {
@@ -138,10 +155,16 @@ function getConcert() {
             console.log(error.config);
         });
     } else {
-        console.log("\nLooks like you forgot to enter an artist. Try again!\n");
+
+        renderInfo = `\nLooks like you forgot to enter an artist. Try again!\n\n------------------------\n`;
+
+        console.log(renderInfo);
+
+        writeFile();
     }
 };
 
+// This function reads the random.txt file, turns the text therein into an array, grabs the array members to create a command and serach term, then uses those to call the appropriate function 
 function readTxt() {
 
     fs.readFile("random.txt", "utf8", function(error, data) {
@@ -155,11 +178,37 @@ function readTxt() {
         command = array[0];
         searchTerm = array[1].split('"').join("");
 
-        getSong(searchTerm);
+        switch(command) {
+            case "movie-this": 
+                return getMovie(searchTerm);
+            case "concert-this": 
+                return getConcert(searchTerm);
+            case "spotify-this-song": 
+                return getSong(searchTerm);
+            default:
+                console.log("Choose a valid action.");
+                return false;
+        };
 
     });
 };
 
+// This function writes the results from any search to a file called log.txt 
+function writeFile() {
+    fs.appendFile("log.txt", renderInfo, function(err) {
+
+        // If an error was experienced we will log it.
+        if (err) {
+        return console.log(err);
+        }
+    
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        console.log(`\nContent added to log.txt file!\n`);
+    
+    });
+};
+
+// This switchcase takes the command variable and creates cases in which it will run certain functions passing in the user's search term input. If the user's command is not stated as a case here, the user will be directed to write a valid command.
 switch(command) {
     case "movie-this": 
         return getMovie(searchTerm);
